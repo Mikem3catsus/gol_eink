7ee1309ce8444f848ed582838682bb1194008791
Mikem3catsus <mikem@3cats.us>
2021-01-20 22:06:55 -0800

    Refactored the loop and some other code into smaller functions to aid readability

--- main.ts
+++ main.ts
@@ -1,50 +1,55 @@
-/**
- * Sorry about the variable names. May come back
- * 
- * and rename everything after the conversion
- * 
- * between blocks and javascript changed names and
- * 
- * formatting significantly.
- */
-let neighbor_count = 0
-let y_inc = 0
-let y_dec = 0
-let x_inc = 0
-let x_dec = 0
-let x = 0
-let y = 0
-// Set the width and height to be 1/2 the number of pixels for 2 reasons:
-// 1. There isn't enough memory for full-sized arrays
-// 2. The individual pixels are so small it doesn't look good anyway.
-let width = 250 / 2
-let height = 122 / 2
-// Set some values so the typing works
-let live_array = [[0, 0], [0, 0]]
-let next_array = [[0, 0], [0, 0]]
-// build out the 2d arrays that represent the "frame" or "grid"
-for (let index = 0; index <= width - 1; index++) {
-    let column2: number[] = []
-    let column: number[] = []
-    for (let index2 = 0; index2 <= height - 1; index2++) {
-        column[index2] = 0
-        column2[index2] = 0
+function CopyNextArrayToLiveArray () {
+    for (let x3 = 0; x3 <= width - 1; x3++) {
+        for (let y3 = 0; y3 <= height - 1; y3++) {
+            live_array[x3][y3] = next_array[x3][y3]
+        }
     }
-    live_array[index] = column
-    next_array[index] = column2
 }
-// build a flipper
-live_array[2][2] = 1
-live_array[2][1] = 1
-live_array[2][3] = 1
-// and a flyer that will eventually crash into the flipper
-live_array[11][10] = 1
-live_array[12][11] = 1
-live_array[13][11] = 1
-live_array[13][10] = 1
-live_array[13][9] = 1
-basic.forever(function () {
-    // Compute the next frame
+function UpdateInkyBit () {
+    inkybit.clear()
+    for (let inky_x = 0; inky_x <= width - 1; inky_x++) {
+        for (let inky_y = 0; inky_y <= height - 1; inky_y++) {
+            if (live_array[inky_x][inky_y] == 1) {
+                inkybit.drawRectangle(
+                inky_x * 2,
+                inky_y * 2,
+                2,
+                2,
+                inkybit.Color.Black,
+                false
+                )
+            }
+        }
+    }
+    inkybit.show()
+}
+function CreateFlipper (flipper_x: number, flipper_y: number) {
+    live_array[flipper_x + 1][flipper_y] = 1
+    live_array[flipper_x + 1][flipper_y + 1] = 1
+    live_array[flipper_x + 1][flipper_y + 2] = 1
+}
+function UpdateLEDs () {
+    // display on LEDs
+    for (let led_x = 0; led_x <= 4; led_x++) {
+        for (let led_y = 0; led_y <= 4; led_y++) {
+            if (live_array[led_x][led_y] == 1) {
+                led.plot(led_x, led_y)
+            } else {
+                led.unplot(led_x, led_y)
+            }
+        }
+    }
+}
+
+function CreateFlyer (flyer_x: number, flyer_y: number) {
+    live_array[flyer_x][flyer_y + 1] = 1
+    live_array[flyer_x + 1][flyer_y + 2] = 1
+    live_array[flyer_x + 2][flyer_y + 2] = 1
+    live_array[flyer_x + 2][flyer_y + 1] = 1
+    live_array[flyer_x + 2][flyer_y] = 1
+}
+
+function ComputeNextArray () {
     for (let x2 = 0; x2 <= width - 1; x2++) {
         for (let y2 = 0; y2 <= height - 1; y2++) {
             // Javascript doesn't % negative numbers to positive numbers, so
@@ -69,39 +74,39 @@ basic.forever(function () {
             }
         }
     }
-    // copy next frame to current frame
-    for (let x3 = 0; x3 <= width - 1; x3++) {
-        for (let y3 = 0; y3 <= height - 1; y3++) {
-            live_array[x3][y3] = next_array[x3][y3]
-        }
+}
+let neighbor_count = 0
+let y_inc = 0
+let y_dec = 0
+let x_inc = 0
+let x_dec = 0
+let next_array: number[][] = []
+let live_array: number[][] = []
+let height = 0
+let width = 0
+// Set the width and height to be 1/2 the number of pixels for 2 reasons:
+// 1. There isn't enough memory for full-sized arrays
+// 2. The individual pixels are so small it doesn't look good anyway.
+width = 250 / 2
+height = 122 / 2
+// build out the 2d arrays that represent the "frame" or "grid"
+for (let index = 0; index <= width - 1; index++) {
+    let column2: number[] = []
+    let column: number[] = []
+    for (let index2 = 0; index2 <= height - 1; index2++) {
+        column[index2] = 0
+        column2[index2] = 0
     }
-    // display on LEDs
-    for (let x4 = 0; x4 <= 4; x4++) {
-        for (let y4 = 0; y4 <= 4; y4++) {
-            if (live_array[x4][y4] == 1) {
-                led.plot(x4, y4)
-            } else {
-                led.unplot(x4, y4)
-            }
-        }
-    }
-    // display on eink
-    inkybit.clear()
-    for (let x5 = 0; x5 <= width - 1; x5++) {
-        for (let y5 = 0; y5 <= height - 1; y5++) {
-            if (live_array[x5][y5] == 1) {
-                inkybit.drawRectangle(
-                x5 * 2,
-                y5 * 2,
-                2,
-                2,
-                inkybit.Color.Black,
-                false
-                )
-            }
-        }
-    }
-    inkybit.show()
+    live_array[index] = column
+    next_array[index] = column2
+}
+CreateFlipper(1, 1)
+CreateFlyer(10, 10)
+basic.forever(function () {
+    ComputeNextArray()
+    CopyNextArrayToLiveArray()
+    UpdateLEDs()
+    UpdateInkyBit()
     // the inkybit.clear() takes ~11 seconds and we want to show the
     // completed scene for a reasonable amount of time. 30 seconds
     // seemed about right.
